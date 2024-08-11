1a333cbea12f8ca080644e2c155af159a46842f4
Michael Lei <michaelleie0ca@gmail.com>
2024-08-11 12:59:20 -0700

    Add kinde auth
    Co-authored-by: Scott Chen <scott.chen1117@gmail.com>

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import type { ReactNode } from "react";
+
+export default async function AuthLayout({
+	children,
+}: {
+	children: ReactNode;
+}) {
+	const { getUser } = getKindeServerSession();
+	const user = await getUser();
+	if (!user) {
+		redirect("/api/auth/login");
+	}
+	return children;
+}
--- src/app/(marketing)/navbar.tsx
+++ src/app/(marketing)/navbar.tsx
@@ -1,14 +1,26 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import {
+	getKindeServerSession,
+	LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
-export default function Navbar() {
+export default async function Navbar() {
+	const { getUser } = getKindeServerSession();
+
+	const user = await getUser();
+
 	return (
 		<header>
 			<div className="container flex justify-between relative z-10 border-b py-6 items-center">
 				<Logo href="/" />
 				<Button asChild>
-					<Link href="/dashboard">Dashboard</Link>
+					{user ? (
+						<Link href="/dashboard">Dashboard</Link>
+					) : (
+						<LoginLink>Login</LoginLink>
+					)}
 				</Button>
 			</div>
 		</header>
