2e13d712897f6a4b3922b8d6265a67685d5e1df6
Michael Lei <michaelleie0ca@gmail.com>
2024-09-08 20:15:06 -0700

    Add resume page
    Co-authored-by: Scott Chen <scott.chen1117@gmail.com>

---
+++ src/app/(auth)/dashboard/resumes/page.tsx
@@ -0,0 +1,10 @@
+import DashboardHeading from "@/components/page-heading";
+import { FilesIcon } from "lucide-react";
+
+export default function ResumesPage() {
+  return (
+    <>
+      <DashboardHeading title="Resumes" icon={FilesIcon} />
+    </>
+  );
+}
